97261144848a9507e46db8db93d0cd57553e7291
ken5scal <kengoscal@gmail.com>
2017-02-10 04:16:50 +0900

    Update

--- src/main/java/googleio/demo/UnusedActivityDetector.java
+++ src/main/java/googleio/demo/UnusedActivityDetector.java
@@ -151,7 +151,7 @@ public class UnusedActivityDetector extends Detector implements Detector.XmlScan
 
         System.out.println(constructor.getParameterList().getParametersCount());
         for (int i = 0; i < constructor.getParameterList().getParametersCount(); i++) {
-            System.out.println(constructor.getParameterList().getParameters()[i]);
+            System.out.println(constructor.getTypeParameters()[i]);
         }
     }
 
