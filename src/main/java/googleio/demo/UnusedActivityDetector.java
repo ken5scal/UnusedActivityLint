2335de2def14c095c9048d43a60d43ce903c08e5
ken5scal <kengoscal@gmail.com>
2017-02-14 12:53:40 +0900

    Update

--- src/main/java/googleio/demo/UnusedActivityDetector.java
+++ src/main/java/googleio/demo/UnusedActivityDetector.java
@@ -1,25 +1,21 @@
 package googleio.demo;
 
 import com.android.annotations.NonNull;
-import com.android.annotations.Nullable;
-import com.android.tools.lint.client.api.JavaEvaluator;
-import com.android.tools.lint.client.api.JavaParser;
 import com.android.tools.lint.detector.api.Category;
 import com.android.tools.lint.detector.api.Context;
 import com.android.tools.lint.detector.api.Detector;
 import com.android.tools.lint.detector.api.Implementation;
 import com.android.tools.lint.detector.api.Issue;
 import com.android.tools.lint.detector.api.JavaContext;
+import com.android.tools.lint.detector.api.LintUtils;
 import com.android.tools.lint.detector.api.Location;
 import com.android.tools.lint.detector.api.Scope;
 import com.android.tools.lint.detector.api.Severity;
+import com.android.tools.lint.detector.api.Speed;
 import com.android.tools.lint.detector.api.XmlContext;
 import com.intellij.psi.JavaElementVisitor;
-import com.intellij.psi.PsiClass;
-import com.intellij.psi.PsiMethod;
-import com.intellij.psi.PsiMethodCallExpression;
-import com.intellij.psi.PsiNewExpression;
-import com.intellij.psi.PsiParameter;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiJavaCodeReferenceElement;
 
 import org.w3c.dom.Element;
 
@@ -33,9 +29,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import lombok.ast.AstVisitor;
-import lombok.ast.ConstructorInvocation;
-
 import static com.android.SdkConstants.ANDROID_URI;
 import static com.android.SdkConstants.ATTR_NAME;
 import static com.android.SdkConstants.TAG_ACTIVITY;
@@ -44,8 +37,8 @@ import static com.android.SdkConstants.TAG_ACTIVITY;
 /**
  * Checks for an activity that has never called
  */
-public class UnusedActivityDetector extends Detector implements Detector.XmlScanner, Detector.JavaPsiScanner {
-
+//public class UnusedActivityDetector extends Detector implements Detector.XmlScanner, Detector.JavaPsiScanner {
+public class UnusedActivityDetector extends Detector implements Detector.XmlScanner, Detector.JavaScanner {
     private static final Implementation IMPLEMENTATION = new Implementation(
             UnusedActivityDetector.class,
             EnumSet.of(Scope.MANIFEST, Scope.JAVA_FILE)
@@ -81,11 +74,6 @@ public class UnusedActivityDetector extends Detector implements Detector.XmlScan
     }
 
     @Override
-    public boolean appliesTo(@NonNull Context context, @NonNull File file) {
-        return true;
-    }
-
-    @Override
     public void beforeCheckProject(@NonNull Context context) {
         System.out.println("beforeCheckProject");
         if (context.getPhase() == 1) {
@@ -93,7 +81,6 @@ public class UnusedActivityDetector extends Detector implements Detector.XmlScan
             mReferences = new HashSet<String>(NUM_OF_ACTIVITIES);
             mActivities = new ArrayList<String>();
         }
-
     }
 
     @Override
@@ -117,84 +104,73 @@ public class UnusedActivityDetector extends Detector implements Detector.XmlScan
         }
     }
 
-    @Nullable
-    @Override
-    public List<String> getApplicableMethodNames() {
-        System.out.println("getApplicableMethodNames");
-        //        list.add(START_ACTIVITY_FOR_RESULT);
-//        list.add(START_ACTIVITY_FROM_CHILD);
-//        list.add(START_ACTIVITY_FROM_FRAGMENT);
-//        list.add(START_ACTIVITY_IF_NEEDED);
-        return Collections.singletonList(START_ACTIVITY);
-    }
+//    @Nullable
+//    @Override
+//    public List<String> getApplicableMethodNames() {
+//        System.out.println("getApplicableMethodNames");
+//        //        list.add(START_ACTIVITY_FOR_RESULT);
+////        list.add(START_ACTIVITY_FROM_CHILD);
+////        list.add(START_ACTIVITY_FROM_FRAGMENT);
+////        list.add(START_ACTIVITY_IF_NEEDED);
+//        return Collections.singletonList(START_ACTIVITY);
+//    }
 
-    @Override
-    public void visitMethod(JavaContext context, JavaElementVisitor visitor, PsiMethodCallExpression call, PsiMethod method) {
-        JavaEvaluator evaluator = context.getEvaluator();
-        System.out.println("visitMethod");
-        if (evaluator.methodMatches(method, ACTIVITY, true, "android.content.Intent")) {
-            startActivityLocation.add(context.getNameLocation(call));
-        }
-    }
+//    @Override
+//    public void visitMethod(JavaContext context, JavaElementVisitor visitor, PsiMethodCallExpression call, PsiMethod method) {
+//        JavaEvaluator evaluator = context.getEvaluator();
+//        System.out.println("visitMethod");
+//        if (evaluator.methodMatches(method, ACTIVITY, true, "android.content.Intent")) {
+//            startActivityLocation.add(context.getNameLocation(call));
+//        }
+//    }
+//
+//    @Override
+//    public List<String> getApplicableConstructorTypes() {
+//        return Collections.singletonList("android.content.Intent");
+//    }
+//
+//    @Override
+//    public void visitConstructor(JavaContext context, JavaElementVisitor visitor, PsiNewExpression node, PsiMethod constructor) {
+//        System.out.println("visitConstructor");
+////        System.out.println("node to string: " + node.toString());
+////        System.out.println("node get text: " + node.getText());
+////        System.out.println("resolve constructor: " + node.resolveConstructor());
+////        System.out.println("constructor param list: " + constructor.getParameterList());
+////        constructor.getParameterList().getParameters()
+////        constructor.getTypeParameterList().getTypeParameters()
+////        System.out.println(constructor.getTypeParameterList().getTypeParameters()[0]);
+////        System.out.println(constructor.getTypeParameterList().getTypeParameters()[1]);
+//
+//        System.out.println(constructor.getParameterList().getParametersCount());
+//        for (int i = 0; i < constructor.getParameterList().getParametersCount(); i++) {
+//            System.out.println(Integer.toString(i) + constructor.getParameterList().getParameters()[i]);
+//        }
+//        PsiParameter param = constructor.getParameterList().getParameters()[1];
+////        JavaParser.TypeDescriptor
+//
+//    }
 
-    @Override
-    public List<String> getApplicableConstructorTypes() {
-        return Collections.singletonList("android.content.Intent");
-    }
-
-    @Override
-    public void visitConstructor(JavaContext context, JavaElementVisitor visitor, PsiNewExpression node, PsiMethod constructor) {
-        System.out.println("visitConstructor");
-//        System.out.println("node to string: " + node.toString());
-//        System.out.println("node get text: " + node.getText());
-//        System.out.println("resolve constructor: " + node.resolveConstructor());
-//        System.out.println("constructor param list: " + constructor.getParameterList());
-//        constructor.getParameterList().getParameters()
-//        constructor.getTypeParameterList().getTypeParameters()
-//        System.out.println(constructor.getTypeParameterList().getTypeParameters()[0]);
-//        System.out.println(constructor.getTypeParameterList().getTypeParameters()[1]);
-
-        System.out.println(constructor.getParameterList().getParametersCount());
-        for (int i = 0; i < constructor.getParameterList().getParametersCount(); i++) {
-            System.out.println(Integer.toString(i) + constructor.getParameterList().getParameters()[i]);
-        }
-        PsiParameter param = constructor.getParameterList().getParameters()[1];
-//        JavaParser.TypeDescriptor
-
-    }
-
-    @Override
-    public void visitConstructor(JavaContext context, AstVisitor visitor, ConstructorInvocation node, JavaParser.ResolvedMethod constructor) {
-        super.visitConstructor(context, visitor, node, constructor);
-    }
-
-    @Override
-    public void checkClass(JavaContext context, PsiClass node) {
-        System.out.println("checkClass");
-        if (node == null) {
-            return;
-        }
-
-        boolean found = false;
-        for (PsiMethod constructor : node.getConstructors()) {
-//            if (isIntentConstructor(constructor)) {
-//                found = true;
-//                break;
-//            }
-        }
-    }
+//    @Override
+//    public void checkClass(JavaContext context, PsiClass node) {
+//        System.out.println("checkClass");
+//        if (node == null) {
+//            return;
+//        }
+//
+//        boolean found = false;
+//        for (PsiMethod constructor : node.getConstructors()) {
+////            if (isIntentConstructor(constructor)) {
+////                found = true;
+////                break;
+////            }
+//        }
+//    }
 
 //    private static boolean isIntentConstructor(PsiMethod method) {
 //        // Accept
 //        //  android.content.Intent
 //        return false;
 //    }
-
-    @Override
-    public void beforeCheckFile(@NonNull Context context) {
-//        File file = context.file;
-//        boolean isJavaFile = LintUtils.isXmlFile()
-    }
 
     @Override
     public void afterCheckFile(Context context) {
@@ -217,6 +193,11 @@ public class UnusedActivityDetector extends Detector implements Detector.XmlScan
     }
 
     @Override
+    public boolean appliesTo(@NonNull Context context, @NonNull File file) {
+        return true;
+    }
+
+    @Override
     public void visitElement(XmlContext context, Element activityElement) {
         System.out.println("visitElement");
         if (activityElement.getTagName().equals(TAG_ACTIVITY)) {
@@ -230,5 +211,31 @@ public class UnusedActivityDetector extends Detector implements Detector.XmlScan
 //            context.report(ISSUE, activityElement, context.getLocation(activityElement),
 //                    "Missing required attribute 'exampleString'");
 //        }
+    }
+
+
+    @NonNull
+    @Override
+    public Speed getSpeed() {
+        return Speed.FAST;
+    }
+
+    // ---- Implements JavaScanner ----
+    @Override
+    public void beforeCheckFile(@NonNull Context context) {
+        System.out.println("beforeCheckFile");
+        File file = context.file;
+        if (LintUtils.isXmlFile(file) || LintUtils.isBitmapFile(file)) {
+            return;
+        }
+
+        String fileName = file.getName();
+        String parentName = file.getParentFile().getName();
+        System.out.printf("FileName:  %s\nParentName: %s\n", fileName, parentName);
+    }
+
+    @Override
+    public void visitReference(JavaContext context, JavaElementVisitor visitor, PsiJavaCodeReferenceElement reference, PsiElement referenced) {
+        super.visitReference(context, visitor, reference, referenced);
     }
 }
