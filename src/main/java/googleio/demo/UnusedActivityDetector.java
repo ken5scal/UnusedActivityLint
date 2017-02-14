4a465fa876065af393cf04861e8291f2356dd71b
ken5scal <kengoscal@gmail.com>
2017-02-14 21:42:43 +0900

    Remove Unused package

--- src/main/java/googleio/demo/UnusedActivityDetector.java
+++ src/main/java/googleio/demo/UnusedActivityDetector.java
@@ -11,7 +11,6 @@ import com.android.tools.lint.detector.api.Location;
 import com.android.tools.lint.detector.api.Scope;
 import com.android.tools.lint.detector.api.Severity;
 import com.android.tools.lint.detector.api.XmlContext;
-import com.intellij.psi.JavaElementVisitor;
 
 import org.w3c.dom.Element;
 
